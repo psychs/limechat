f695a06f5a91767e588ca7b8fd3d085273495f15
psychs <psychs@c9ccf570-da32-0410-b515-c1469a46b0c3>
2007-07-09 07:35:47 +0000

    Implementing server dialog.

--- ruby/dialog/serverdialog.rb
+++ ruby/dialog/serverdialog.rb
@@ -12,7 +12,7 @@ class ServerDialog < OSX::NSObject
   ib_mapped_outlet :nameText, :hostCombo, :passwordText, :nickText, :usernameText, :realnameText, :encodingCombo, :auto_connectCheck
   ib_mapped_int_outlet :portText
   ib_outlet :leaveCommentText, :userinfoText, :invisibleCheck, :loosenNickLengthCheck, :nickLengthText
-  ib_outlet :channelsTable
+  ib_outlet :channelsTable, :addButton, :editButton, :upButton, :downButton
   ib_outlet :okButton
   
   def initialize
@@ -31,7 +31,8 @@ class ServerDialog < OSX::NSObject
     @channelsTable.setDoubleAction('tableView_doubleClicked:')
     @window.setTitle("New Server") if uid < 0
     load
-    update
+    update_connection_page
+    update_channels_page
     show
   end
   
@@ -67,10 +68,10 @@ class ServerDialog < OSX::NSObject
   end
   
   def controlTextDidChange(n)
-    update
+    update_connection_page
   end
   
-  def update
+  def update_connection_page
     name = @nameText.stringValue.to_s
     host = @hostCombo.stringValue.to_s
     port = @portText.stringValue.to_s
@@ -78,6 +79,32 @@ class ServerDialog < OSX::NSObject
     username = @usernameText.stringValue.to_s
     realname = @realnameText.stringValue.to_s
     @okButton.setEnabled(!name.empty? && !host.empty? && port.to_i > 0 && !nick.empty? && !username.empty? && !realname.empty?)
+  end
+  
+  def update_channels_page
+    t = @channelsTable
+    sel = t.selectedRows[0]
+    unless sel
+      @editButton.setEnabled(false)
+      @upButton.setEnabled(false)
+      @downButton.setEnabled(false)
+    else
+      @editButton.setEnabled(true)
+      if sel == 0
+        @upButton.setEnabled(false)
+        @downButton.setEnabled(true)
+      elsif sel == @c.channels.length - 1
+        @upButton.setEnabled(true)
+        @downButton.setEnabled(false)
+      else
+        @upButton.setEnabled(true)
+        @downButton.setEnabled(true)
+      end
+    end
+  end
+  
+  def reload_table
+    @channelsTable.reloadData
   end
   
   def numberOfRowsInTableView(sender)
@@ -110,7 +137,124 @@ class ServerDialog < OSX::NSObject
     end
   end
   
+  def tableViewSelectionDidChange(n)
+    update_channels_page
+  end
+  
   def tableView_doubleClicked(sender)
-    puts 'double click'
+    onEdit(sender)
+  end
+  
+  def onAdd(sender)
+    puts 'add'
+  end
+  
+  def onEdit(sender)
+    sel = @channelsTable.selectedRows[0]
+    return unless sel
+    puts 'edit'
+  end
+  
+  def onUp(sender)
+    sel = @channelsTable.selectedRows[0]
+    return unless sel
+    return if sel == 0
+    i = @c.channels.delete_at(sel)
+    sel -= 1
+    @c.channels.insert(sel, i)
+    @channelsTable.select(sel)
+    reload_table
+  end
+  
+  def onDown(sender)
+    sel = @channelsTable.selectedRows[0]
+    return unless sel
+    return if sel == @c.channels.length - 1
+    i = @c.channels.delete_at(sel)
+    sel += 1
+    @c.channels.insert(sel, i)
+    @channelsTable.select(sel)
+    reload_table
   end
 end
+
+=begin
+#import <Cocoa/Cocoa.h>
+
+#define TableRowType @"row"
+#define TableRowTypes [NSArray arrayWithObjects:@"row",nil]
+
+@interface AppController : NSObject {
+
+	IBOutlet id sampleTableView;
+
+	NSMutableArray *tableArray;
+}
+-(void)awakeFromNib;
+-(BOOL)tableView:(NSTableView *)tv writeRows:(NSArray*)rows toPasteboard:(NSPasteboard*)pboard;
+-(NSDragOperation)tableView:(NSTableView*)tv validateDrop:(id <NSDraggingInfo>)info proposedRow:(int)row proposedDropOperation:(NSTableViewDropOperation)op;
+-(BOOL)tableView:(NSTableView*)tv acceptDrop:(id <NSDraggingInfo>)info row:(int)row dropOperation:(NSTableViewDropOperation)op;
+@end
+
+@implementation AppController
+
+-(void)awakeFromNib{
+	[sampleTableView registerForDraggedTypes:TableRowTypes];
+}
+
+-(BOOL)tableView:(NSTableView *)tv writeRows:(NSArray*)rows toPasteboard:(NSPasteboard*)pboard{
+	[pboard declareTypes:TableRowTypes owner:self];
+	[pboard setPropertyList:rows forType:TableRowType];
+	return YES;
+}
+
+-(NSDragOperation)tableView:(NSTableView*)tv validateDrop:(id <NSDraggingInfo>)info proposedRow:(int)row proposedDropOperation:(NSTableViewDropOperation)op{
+	NSPasteboard *pboard=[info draggingPasteboard];
+
+	if (op == NSTableViewDropAbove && [pboard availableTypeFromArray:TableRowTypes] != nil) {
+		return NSDragOperationGeneric;
+	} else {
+		return NSDragOperationNone;
+	}
+}
+
+-(BOOL)tableView:(NSTableView*)tv acceptDrop:(id <NSDraggingInfo>)info row:(int)row dropOperation:(NSTableViewDropOperation)op{
+	NSPasteboard *pboard=[info draggingPasteboard];
+	NSEnumerator *e=[[pboard propertyListForType:TableRowType] objectEnumerator];
+	NSNumber *number;
+	NSMutableArray *upperArray=[NSMutableArray arrayWithArray:[tableArray subarrayWithRange:NSMakeRange(0,row)]];
+	NSMutableArray *lowerArray=[NSMutableArray arrayWithArray:[tableArray subarrayWithRange:NSMakeRange(row,([tableArray count] - row))]];
+	NSMutableArray *middleArray=[NSMutableArray arrayWithCapacity:0];
+	id object;
+	int i;
+
+	if (op == NSTableViewDropAbove && [pboard availableTypeFromArray:TableRowTypes] != nil) {
+
+		while ((number=[e nextObject]) != nil) {
+			object=[tableArray objectAtIndex:[number intValue]];
+			[middleArray addObject:object];
+			[upperArray removeObject:object];
+			[lowerArray removeObject:object];
+		}
+
+		[tableArray removeAllObjects];
+
+		[tableArray addObjectsFromArray:upperArray];
+		[tableArray addObjectsFromArray:middleArray];
+		[tableArray addObjectsFromArray:lowerArray];
+
+		[sampleTableView reloadData];
+		[sampleTableView deselectAll:nil];
+	
+		for (i=[upperArray count];i<([upperArray count] + [middleArray count]);i++) {
+			[sampleTableView selectRow:i byExtendingSelection:[sampleTableView allowsMultipleSelection]];
+		}
+
+		return YES;
+	} else {
+		return NO;
+	}
+}
+
+@end
+=end
