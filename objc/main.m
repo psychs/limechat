84efefabadc95948545e79c738288d8f5957d1e3
psychs <psychs@c9ccf570-da32-0410-b515-c1469a46b0c3>
2007-08-13 07:21:11 +0000

    Clean up.

--- objc/main.m
+++ objc/main.m
@@ -1,24 +1,6 @@
 #import <RubyCocoa/RBRuntime.h>
 
-/*
-void print_encodings()
-{
-  const NSStringEncoding *encodings = [NSString availableStringEncodings];
-  NSMutableString *str = [[NSMutableString alloc] init];
-  NSStringEncoding encoding;
-  while ((encoding = *encodings++) != 0) {
-      [str appendFormat: @"\n%d, %@, %@",
-          encoding,
-          [NSString localizedNameOfStringEncoding:encoding],
-          CFStringConvertEncodingToIANACharSetName(CFStringConvertNSStringEncodingToEncoding(encoding))
-      ];
-  }
-  NSLog(@"%@", str);
-}
-*/
-
 int main(int argc, const char* argv[])
 {
-//  setenv("RUBYCOCOA_THREAD_HOOK_DISABLE", "1", 1);
   return RBApplicationMain("rb_main.rb", argc, argv);
 }
