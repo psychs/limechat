b39c6623370e5cb6cd9742aafb8896a450516596
psychs <psychs@c9ccf570-da32-0410-b515-c1469a46b0c3>
2007-12-06 19:54:14 +0000

    Changed to set ENV['HOME'] at starting up.

--- objc/main.m
+++ objc/main.m
@@ -3,5 +3,8 @@
 int main(int argc, const char* argv[])
 {
   //setenv("RUBYCOCOA_THREAD_HOOK_DISABLE", "1", 1);
+  NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+  setenv("HOME", [[@"~/" stringByExpandingTildeInPath] UTF8String], 0);
+  [pool release];
   return RBApplicationMain("rb_main.rb", argc, argv);
 }
