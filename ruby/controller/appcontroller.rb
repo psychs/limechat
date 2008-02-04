ee3153d16e2415cb0931f59a86dbfe617e787979
psychs <psychs@c9ccf570-da32-0410-b515-c1469a46b0c3>
2008-02-04 15:20:23 +0000

    Added note for the encoding setting.

--- ruby/controller/appcontroller.rb
+++ ruby/controller/appcontroller.rb
@@ -12,6 +12,8 @@ class AppController < NSObject
   def awakeFromNib
     SACrashReporter.submit
     
+    raise 1
+    
     @pref = Preferences.new
     #FileUtils.mkpath(@pref.gen.transcript_folder.expand_path) rescue nil
     
