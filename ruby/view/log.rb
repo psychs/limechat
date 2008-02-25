9bed4b0e93397a8328129f338554633bb3dd2881
psychs <psychs@c9ccf570-da32-0410-b515-c1469a46b0c3>
2008-02-25 07:43:14 +0000

    Added DOCTYPE and some meta tags to the template for log views.

--- ruby/view/log.rb
+++ ruby/view/log.rb
@@ -396,11 +396,15 @@ class LogController < NSObject
       override_style = ''
     end
     <<-EOM
+      <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
       <html>
       <head>
-      <style>#{DEFAULT_CSS}</style>
-      <style><!--#{style}--></style>
-      <style>#{override_style}</style>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta http-equiv="Content-Script-Type" content="text/javascript">
+        <meta http-equiv="Content-Style-Type" content="text/css">
+        <style>#{DEFAULT_CSS}</style>
+        <style><!--#{style}--></style>
+        <style>#{override_style}</style>
       </head>
       <body class="#{body_class}"></body>
       </html>
