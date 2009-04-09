98ba4fd69b584ae235d148cfe087d12edf99d8a1
Satoshi Nakagawa <psychs@limechat.net>
2009-04-09 18:37:36 +0900

    added a command, "/close peer" to close a talk

--- ruby/irc/unit.rb
+++ ruby/irc/unit.rb
@@ -448,6 +448,17 @@ class IRCUnit < NSObject
         @world.select(c)
       end
       return true
+    when :close
+      target = s.token!
+      if target.empty?
+        c = @world.selchannel
+      else
+        c = find_channel(target)
+      end
+      if c && c.talk?
+        @world.destroy_channel(c)
+      end
+      return true
     when :timer
       interval = s.token!
       if interval =~ /^\d+$/
