5e612bc73c5a99ab6940082f946455658e8cfe0d
Satoshi Nakagawa <psychs@limechat.net>
2010-03-23 18:24:33 +0900

    Added open browser in background option. Thanks to evanphx. http://github.com/evanphx/limechat/commit/76ba02f20e4337ea92180fb49cfcd8b08c84ec24

--- ruby/lib/urlopener.rb
+++ ruby/lib/urlopener.rb
@@ -1,11 +1,18 @@
 # Created by Satoshi Nakagawa.
 # You can redistribute it and/or modify it under the Ruby's license or the GPL2.
 
+require 'preferences'
+
 module UrlOpener
   
   def openUrl(str)
-    urls = [OSX::NSURL.URLWithString(str)]
-    OSX::NSWorkspace.sharedWorkspace.openURLs_withAppBundleIdentifier_options_additionalEventParamDescriptor_launchIdentifiers(urls, nil, NSWorkspaceLaunchWithoutActivation, nil, nil)
+    if preferences.general.open_browser_in_background
+      urls = [OSX::NSURL.URLWithString(str)]
+      OSX::NSWorkspace.sharedWorkspace.openURLs_withAppBundleIdentifier_options_additionalEventParamDescriptor_launchIdentifiers(urls, nil, NSWorkspaceLaunchWithoutActivation, nil, nil)
+    else
+      url = OSX::NSURL.URLWithString(str)
+      OSX::NSWorkspace.sharedWorkspace.openURL(url)
+    end
   end
   
   extend self
