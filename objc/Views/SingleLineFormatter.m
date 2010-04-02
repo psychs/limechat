6020f3971dbf6d52b47cdc25d68350a59c47c9f2
Satoshi Nakagawa <psychs@limechat.net>
2010-04-02 15:59:47 +0900

    implemented single line formatter in objc

---
+++ objc/Views/SingleLineFormatter.m
@@ -0,0 +1,31 @@
+#import "SingleLineFormatter.h"
+
+
+@implementation SingleLineFormatter
+
+- (NSString*)stringForObjectValue:(id)str
+{
+	str = [str stringByReplacingOccurrencesOfString:@"\r" withString:@" "];
+	return [str stringByReplacingOccurrencesOfString:@"\n" withString:@" "];
+}
+
+- (BOOL)getObjectValue:(id*)obj forString:(NSString*)str errorDescription:(NSString**)err
+{
+	str = [str stringByReplacingOccurrencesOfString:@"\r" withString:@" "];
+	str = [str stringByReplacingOccurrencesOfString:@"\n" withString:@" "];
+	*obj = str;
+	return YES;
+}
+
+- (BOOL)isPartialStringValid:(NSString *)str newEditingString:(NSString **)newString errorDescription:(NSString **)err
+{
+	NSRange r = [str rangeOfCharacterFromSet:[NSCharacterSet newlineCharacterSet]];
+	if (r.location == NSNotFound) return YES;
+	
+	str = [str stringByReplacingOccurrencesOfString:@"\r" withString:@" "];
+	str = [str stringByReplacingOccurrencesOfString:@"\n" withString:@" "];
+	*newString = str;
+	return NO;
+}
+
+@end
