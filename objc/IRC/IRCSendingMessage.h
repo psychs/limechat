765fc0391239d927216577fba4c24e45bdcf2daa
Satoshi Nakagawa <psychs@limechat.net>
2010-04-05 00:42:32 +0900

    tweak

--- objc/IRC/IRCSendingMessage.h
+++ objc/IRC/IRCSendingMessage.h
@@ -1,7 +1,7 @@
 // Created by Satoshi Nakagawa.
 // You can redistribute it and/or modify it under the Ruby's license or the GPL2.
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
 
 @interface IRCSendingMessage : NSObject
