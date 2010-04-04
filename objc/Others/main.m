5468f2b4da4d46e7705537ea5aab067dbcd17d7e
Satoshi Nakagawa <psychs@limechat.net>
2010-04-05 00:27:11 +0900

    refactoring

--- objc/Others/main.m
+++ objc/Others/main.m
@@ -7,7 +7,7 @@
 int main(int argc, const char* argv[])
 {
 	NSAutoreleasePool* pool = [NSAutoreleasePool new];
-	setenv("HOME", [[@"~/" stringByExpandingTildeInPath] UTF8String], 0);
+	setenv("HOME", [NSHomeDirectory() UTF8String], 0);
 	[pool drain];
 	return RBApplicationMain("rb_main.rb", argc, argv);
 }
