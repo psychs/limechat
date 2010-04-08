01ec13ec22150f919c7c2a773fa9b7be029470b3
Satoshi Nakagawa <psychs@limechat.net>
2010-04-08 19:02:14 +0900

    do not load RubyCocoa

--- objc/Others/main.m
+++ objc/Others/main.m
@@ -1,13 +1,8 @@
 // Created by Satoshi Nakagawa.
 // You can redistribute it and/or modify it under the Ruby's license or the GPL2.
 
-#import <RubyCocoa/RBRuntime.h>
-
 
 int main(int argc, const char* argv[])
 {
-	NSAutoreleasePool* pool = [NSAutoreleasePool new];
-	setenv("HOME", [NSHomeDirectory() UTF8String], 0);
-	[pool drain];
-	return RBApplicationMain("rb_main.rb", argc, argv);
+	return NSApplicationMain(argc, argv);
 }
