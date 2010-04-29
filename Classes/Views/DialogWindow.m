fd1d54fddd58fa82d1755a48e77a77c25ceb9f58
Satoshi Nakagawa <psychs@limechat.net>
2010-04-30 08:32:22 +0900

    do not process key event while using input manager

--- Classes/Views/DialogWindow.m
+++ Classes/Views/DialogWindow.m
@@ -12,41 +12,44 @@
 {
 	if (keyDelegate) {
 		if ([e type] == NSKeyDown) {
-			int k = [e keyCode];
-			NSUInteger m = [e modifierFlags];
-			BOOL shift = m & NSShiftKeyMask != 0;
-			BOOL ctrl = m & NSControlKeyMask != 0;
-			BOOL alt = m & NSAlternateKeyMask != 0;
-			BOOL cmd = m & NSCommandKeyMask != 0;
-			
-			if (!shift && !ctrl && !alt && !cmd) {
-				// no mods
-				switch (k) {
-					case 53:	// esc
-						if ([keyDelegate respondsToSelector:@selector(dialogWindowEscape)]) {
-							[keyDelegate dialogWindowEscape];
-						}
-						return;
-					case 76:	// enter
-						if ([keyDelegate respondsToSelector:@selector(dialogWindowEnter)]) {
-							[keyDelegate dialogWindowEnter];
-						}
-						return;
+			NSInputManager* im = [NSInputManager currentInputManager];
+			if (!im || !im.markedRange.length) {
+				int k = [e keyCode];
+				NSUInteger m = [e modifierFlags];
+				BOOL shift = m & NSShiftKeyMask != 0;
+				BOOL ctrl = m & NSControlKeyMask != 0;
+				BOOL alt = m & NSAlternateKeyMask != 0;
+				BOOL cmd = m & NSCommandKeyMask != 0;
+				
+				if (!shift && !ctrl && !alt && !cmd) {
+					// no mods
+					switch (k) {
+						case 53:	// esc
+							if ([keyDelegate respondsToSelector:@selector(dialogWindowEscape)]) {
+								[keyDelegate dialogWindowEscape];
+							}
+							return;
+						case 76:	// enter
+							if ([keyDelegate respondsToSelector:@selector(dialogWindowEnter)]) {
+								[keyDelegate dialogWindowEnter];
+							}
+							return;
+					}
 				}
-			}
-			else if (!shift && !ctrl && !alt && cmd || !shift && ctrl && !alt && !cmd) {
-				// no mods
-				switch (k) {
-					case 125:	// down
-						if ([keyDelegate respondsToSelector:@selector(dialogWindowMoveDown)]) {
-							[keyDelegate dialogWindowMoveDown];
-						}
-						return;
-					case 126:	// up
-						if ([keyDelegate respondsToSelector:@selector(dialogWindowMoveUp)]) {
-							[keyDelegate dialogWindowMoveUp];
-						}
-						return;
+				else if (!shift && !ctrl && !alt && cmd || !shift && ctrl && !alt && !cmd) {
+					// cmd or ctrl
+					switch (k) {
+						case 125:	// down
+							if ([keyDelegate respondsToSelector:@selector(dialogWindowMoveDown)]) {
+								[keyDelegate dialogWindowMoveDown];
+							}
+							return;
+						case 126:	// up
+							if ([keyDelegate respondsToSelector:@selector(dialogWindowMoveUp)]) {
+								[keyDelegate dialogWindowMoveUp];
+							}
+							return;
+					}
 				}
 			}
 		}
