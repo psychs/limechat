43c6b5bf590e90010897a804d52100d1bc5bba1b
Satoshi Nakagawa <psychs@limechat.net>
2010-04-30 08:35:40 +0900

    close preferences dialog on ESC

--- Classes/Views/DialogWindow.h
+++ Classes/Views/DialogWindow.h
@@ -6,17 +6,5 @@
 
 @interface DialogWindow : NSWindow
 {
-	id keyDelegate;
 }
-
-@property (nonatomic, assign) id keyDelegate;
-
-@end
-
-
-@interface NSObject (DialogWindowDelegate)
-- (void)dialogWindowEscape;
-- (void)dialogWindowEnter;
-- (void)dialogWindowMoveDown;
-- (void)dialogWindowMoveUp;
 @end
